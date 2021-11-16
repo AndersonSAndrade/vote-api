850006f894983b1801d33a17abe4c9bf38d1e90e
itcodetech <itcodetechs@gmail.com>
2021-11-16 13:06:35 -0300

    Project init Vote API

---
+++ src/main/java/com/ads/voteapi/VoteApiApplication.java
@@ -0,0 +1,13 @@
+package com.ads.voteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ads/voteapi/VoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.voteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
